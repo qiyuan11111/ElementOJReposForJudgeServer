d33cb2dac8500a9b75e2c1f2bfd45bfe8270fd45
qiyuan11111 <1746257500@qq.com>
2021-02-17 21:46:48 +0800

    Update: more

--- config/start.sh
+++ config/start.sh
@@ -10,6 +10,12 @@ mkdir -p /judger/spj
 chown compiler:spj /judger/spj
 chmod 710 /judger/spj
 
+chown 711 /test_case
+chown 700 /test_case/in
+chown 700 /test_case/out
+chown 700 /test_case/sample
+chown 700 /test_case/user_out
+
 mkdir -p /test_case/special
 chown compiler:code /test_case/special
 chmod 711 /test_case/special
